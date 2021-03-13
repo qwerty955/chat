08fc453d9a504d540899358961a4d8676489fb4b
qwerty955 <75160615+qwerty955@users.noreply.github.com>
2021-03-13 11:36:07 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let socket = new WebSocket("wss://fast-dusk-26510.herokuapp.com/");
+
+let prom = prompt("введите ваш ник")
+
+let inp = document.getElementById("inp");
+
+let but = document.getElementById("but");
+
+let t = false;
+socket.addEventListener("open",(e)=>{
+alert("Server open");
+socket.send("просоеденился новый пользователь под ником: "+prom)
+but.addEventListener("click",function () {
+    socket.send(inp.value);
+    console.log("sdg");
+});
+});
+socket.onclose = e => {
+socket.send(`У ${prom} проблемы с интернетом либо он или она отсоеденились от беседы`)
+alert("server close");
+};
+
+socket.onmessage = function (e) {
+document.getElementById("m").innerHTML+=`<p>${prom}: ${e.data}</p>`
+};
+
